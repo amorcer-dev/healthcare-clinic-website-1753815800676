41f7ec2f5197ac8dc12ef8e1cbd5026a04929d42
amorcer-dev <amorcerdev@gmail.com>
2025-07-29 15:03:22 -0400

    Update website

---
+++ components/index.ts
@@ -0,0 +1,6 @@
+export * from './WidgetAboutDoctor';
+export * from './WidgetProfile';
+export * from './WidgetServicesAndConditions';
+export * from './WidgetTestimonialCarousel';
+export * from './WidgetInsurances';
+export * from './WidgetTestimonialCard';
